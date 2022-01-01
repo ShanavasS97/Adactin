d570f3aa20628cf653fe5ba3a1153bff76def879
ShanavasS97 <d2p10shana@gmail.com>
2022-01-01 21:36:58 +0530

    Login

--- src/org/adac/AdactinHotel.java
+++ src/org/adac/AdactinHotel.java
@@ -1,5 +1,11 @@
 package org.adac;
 
 public class AdactinHotel {
+	public static void password() {
+		System.out.println("pass");
+	}
 
+	public static void main(String[] args) {
+		password();
+	}
 }
